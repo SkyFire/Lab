6d183d6bef8e00c532359b9c6fb380e5e57138b0
cooler-SAI <cooler19860212@yahoo.com>
2012-03-24 20:53:16 +0400

    Thorngrin the Tender Boss added for The Botanica Dungeon

--- Development/Dungeons/The Burning Crusade/Tempest Keep-The Botanica.sql
+++ Development/Dungeons/The Burning Crusade/Tempest Keep-The Botanica.sql
@@ -47,11 +47,37 @@ INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`pr
 (@ENTRY,4,1, 'Oh stop your whimpering.',1,0,50,0,0,11072, 'on Player Kill Text'),
 (@ENTRY,5,0, 'I have not yet... begun to...',1,0,100,0,0,11079, 'on Death Text');
 
-
-
-
-
-
+-- Thorngrin the Tender
+SET @ENTRY := 17978;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,4,0,100,7,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Aggro'),
+(@ENTRY,0,1,2,0,0,100,2,12000,18000,18000,24000,11,34659,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Hellfire'),
+(@ENTRY,0,2,0,61,0,100,2,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Cast Hellfire'),
+(@ENTRY,0,3,4,0,0,100,4,12000,18000,18000,24000,11,39131,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Hellfire'),
+(@ENTRY,0,4,0,61,0,100,4,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Cast Hellfire'),
+(@ENTRY,0,5,6,0,0,100,6,6000,10000,24000,30000,11,34661,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Sacrifice'),
+(@ENTRY,0,6,0,61,0,100,6,0,0,0,0,1,2,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Cast Sacrifice'),
+(@ENTRY,0,7,8,0,0,100,6,21000,25000,28000,29000,11,34670,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Enrage'),
+(@ENTRY,0,8,0,61,0,100,6,0,0,0,0,1,3,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Cast Enrage'),
+(@ENTRY,0,9,0,2,0,100,7,0,50,0,0,1,4,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text at 50% HP'),
+(@ENTRY,0,10,0,2,0,100,7,0,20,0,0,1,5,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text at 20% HP'),
+(@ENTRY,0,11,0,5,0,100,6,5000,5000,0,0,1,6,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Player Kill'),
+(@ENTRY,0,12,0,6,0,100,7,0,0,0,0,1,7,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Death');
+-- NPC talk text insert
+SET @ENTRY := 17978;
+DELETE FROM `creature_text` WHERE `entry`=@ENTRY;
+INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
+(@ENTRY,0,0, 'What aggravation is this? You will die!',1,0,100,0,0,11205, 'on Aggro Text'),
+(@ENTRY,1,0, 'Scream while you burn!',1,0,50,0,0,11211, 'on Cast Text'),
+(@ENTRY,1,1, 'I\'ll incinerate you!',1,0,50,0,0,11210, 'on Cast Text'),
+(@ENTRY,2,0, 'Your life will be mine!',1,0,100,0,0,11208, 'on Cast Text'),
+(@ENTRY,3,0, '%s becomes enraged!',2,0,100,0,0,0, 'combat Enrage'),
+(@ENTRY,4,0, 'I revel in your pain!',1,0,100,0,0,11209, 'at 50% HP Text'),
+(@ENTRY,5,0, 'You seek a prize, eh? How about death?',1,0,100,0,0,11206, 'at 20% HP Text'),
+(@ENTRY,6,0, 'I hate to say I told you so...',1,0,100,0,0,11207, 'on Player Kill Text'),
+(@ENTRY,7,0, 'You won\'t... get far.',1,0,100,0,0,11212, 'on Death Text');
 
 
 
