0167cf607370960bb615590bc2e93f4ec83ad01d
cooler-SAI <cooler19860212@yahoo.com>
2012-03-24 22:29:42 +0400

    fixed spell range in The Botanica Dungeon

--- Development/Dungeons/The Burning Crusade/Tempest Keep-The Botanica.sql
+++ Development/Dungeons/The Burning Crusade/Tempest Keep-The Botanica.sql
@@ -317,8 +317,8 @@ DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,14,0,100,2,4000,40,21000,26000,11,34361,0,0,0,0,0,7,0,0,0,0,0,0,0,'Cast Regrowth on Friendlies'),
 (@ENTRY,0,1,0,14,0,100,4,4000,40,21000,26000,11,39125,0,0,0,0,0,7,0,0,0,0,0,0,0,'Cast Regrowth on Friendlies'),
-(@ENTRY,0,2,0,14,0,100,2,1000,0,25000,29000,11,34254,0,0,0,0,0,7,0,0,0,0,0,0,0,'Cast Rejuvenate Plant on Friendlies'),
-(@ENTRY,0,3,0,14,0,100,4,1000,0,25000,29000,11,39126,0,0,0,0,0,7,0,0,0,0,0,0,0,'Cast Rejuvenate Plant on Friendlies'),
+(@ENTRY,0,2,0,14,0,100,2,1000,15,25000,29000,11,34254,0,0,0,0,0,7,0,0,0,0,0,0,0,'Cast Rejuvenate Plant on Friendlies'),
+(@ENTRY,0,3,0,14,0,100,4,1000,15,25000,29000,11,39126,0,0,0,0,0,7,0,0,0,0,0,0,0,'Cast Rejuvenate Plant on Friendlies'),
 (@ENTRY,0,4,0,16,0,100,6,34350,10,16000,25000,11,34350,0,0,0,0,0,7,0,0,0,0,0,0,0,'Cast Nature\'s Rage on Friendlies Missing Buff');
 
 -- Sunseeker Channeler
