add50ec2e70ff006a0cdb6e2ff474e8c71cc9c26
cooler-SAI <cooler19860212@yahoo.com>
2012-03-27 21:23:05 +0400

    Captain Kromcrush script added

--- Development/Dungeons/Classic/Dire Maul.sql
+++ Development/Dungeons/Classic/Dire Maul.sql
@@ -48,8 +48,28 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,0,19,20,2,4,100,3,0,40,0,0,28,22688,0,0,0,0,0,1,0,0,0,0,0,0,0,'Remove Tree Form at 40% HP'),
 (@ENTRY,0,20,0,61,4,100,3,0,0,0,0,39,100,0,0,0,0,0,1,0,0,0,0,0,0,0,'Call for help at 40% HP');
 
+-- Captain Kromcrush
+SET @ENTRY := 14325;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,4,0,100,3,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Aggro'),
+(@ENTRY,0,1,0,9,0,100,2,0,5,9000,10000,11,22859,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Mortal Cleave on Close'),
+(@ENTRY,0,2,0,0,0,100,2,15000,16000,25000,35000,11,22857,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Retaliation'),
+(@ENTRY,0,3,0,0,0,100,2,8000,16000,18000,22000,11,19134,0,0,0,0,0,5,0,0,0,0,0,0,0,'Cast Frightening Shout'),
+(@ENTRY,0,4,5,2,0,100,3,0,50,0,0,11,22860,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Call Reavers at 50% HP'),
+(@ENTRY,0,5,0,61,0,100,3,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Cast Call Reavers at 50% HP'),
+(@ENTRY,0,6,7,2,0,100,3,0,30,0,0,11,8599,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Enrage at 30% HP'),
+(@ENTRY,0,7,0,61,0,100,3,0,0,0,0,1,2,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text at 30% HP');
+-- NPC talk text insert
+SET @ENTRY := 14325;
+DELETE FROM `creature_text` WHERE `entry`=@ENTRY;
+INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
+(@ENTRY,0,0, 'No one get past me and threaten da king!  Ungh, take it!!',14,0,100,0,0,0, 'on Aggro Text'),
+(@ENTRY,1,0, 'Help me crush these punys!',14,0,100,0,0,0, 'on Cast Text'),
+(@ENTRY,2,0, '%s begins to retaliate all attacks against him!',16,0,100,0,0,0, 'on Cast Text');
 	
-	
+
 	   /* Trash Mobs */
 			  
 -- Skeletal Highborne
